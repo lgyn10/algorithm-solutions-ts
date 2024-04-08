3561c52f3e81247636ae3c2a49b5b476b44a881f
ChanGyun Lee <lgyn10@naver.com>
2024-04-08 19:16:46 +0900

    day35 : 행복한 식물 개선 코드 추가

--- day35/행복한식물.ts
+++ day35/행복한식물.ts
@@ -18,5 +18,31 @@ const 식물의상태 = (emotions: number[], orders: number[]): number[] => {
 };
 
 // ver 2. 개선 코드
+// filter 함수를 제거하고 map 함수 내에서 바로 검사할 수 있도록 하여
+// 불필요한 배열 순회를 막았음
+const 식물의상태2 = (emotions: number[], orders: number[]): number[] => {
+  const ans = [];
+  const initialEmotions = [...emotions];
+  for (const order of orders) {
+    // 기분이 좋은 식물의 수
+    let cnt = 0;
+    // 식물 물주기 사이클 진행
+    emotions = emotions.map((v, i) => {
+      const val = order - 1 === i && v > 0 ? initialEmotions[order - 1] : v - 1;
+      if (val > 0) cnt++; // 사이클 진행 후 식물의 기분 척도 검사
+      return val;
+    });
+    ans.push(cnt);
+  }
+  return ans;
+};
 
+console.log(식물의상태([2, 3, 1, 2], [3, 1, 2, 1, 4, 1])); //  [4, 2, 2, 2, 2, 1]
+console.log(식물의상태([5, 5, 5], [1, 2, 1, 2, 3])); // [3, 3, 3, 3, 3]
+console.log(식물의상태([5, 5, 5], [1, 2, 1, 2, 1])); // [3, 3, 3, 3, 2]
 console.log(식물의상태([2, 1, 3, 4, 3], [2, 2, 2, 2, 5, 5, 5])); // [5, 4, 2, 1, 0, 0, 0]
+
+console.log(식물의상태2([2, 3, 1, 2], [3, 1, 2, 1, 4, 1])); //  [4, 2, 2, 2, 2, 1]
+console.log(식물의상태2([5, 5, 5], [1, 2, 1, 2, 3])); // [3, 3, 3, 3, 3]
+console.log(식물의상태2([5, 5, 5], [1, 2, 1, 2, 1])); // [3, 3, 3, 3, 2]
+console.log(식물의상태2([2, 1, 3, 4, 3], [2, 2, 2, 2, 5, 5, 5])); // [5, 4, 2, 1, 0, 0, 0]
