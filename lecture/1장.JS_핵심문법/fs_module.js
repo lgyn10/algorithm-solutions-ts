ebdda13d5654e2d21aa2675b94666b18ec21a463
ChanGyun Lee <lgyn10@naver.com>
2024-04-30 20:09:23 +0900

    4/29 알고리즘 강의 메모

---
+++ lecture/1장.JS_핵심문법/fs_module.js
@@ -0,0 +1,6 @@
+const fs = require('fs');
+
+// const input1 = fs.readFileSync('/dev/stdin').toString().split('\n');
+const input2 = fs.readFileSync('input.txt').toString().split('\n');
+
+console.log(input2); // [ '123', '456', '789 1000' ]
